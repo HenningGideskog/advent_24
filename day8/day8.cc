ddfdab0aa3c4003f8dec5a33ad7f9c5f64c4f9e0
Henning Gideskog <henning.gideskog@gmail.com>
2024-12-13 07:11:59 +0100

    Solved day 8 part 2.

--- day8/day8.cc
+++ day8/day8.cc
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iterator>
+#include <numeric>
 #include <print>
 #include <set>
 #include <sstream>
@@ -43,28 +44,7 @@ vector<size_t> FindAntennaPositions(char f, Grid const& grid)
     return antennas;
 }
 
-void FindAntinode(size_t a1, size_t a2, Grid const& grid, set<size_t>& antinodes)
-{
-    long gridTotalRows = grid.first.size() / grid.second;
-    long gridTotalCols = grid.second;
-    long row1          = a1 / grid.second;
-    long row2          = a2 / grid.second;
-    long col1          = a1 % grid.second;
-    long col2          = a2 % grid.second;
-    long rowDist       = row2 - row1;
-    long colDist       = col2 - col1;
-
-    long rowAntinode = row1 + 2 * rowDist;
-    long colAntinode = col1 + 2 * colDist;
-
-    if (rowAntinode < gridTotalRows && rowAntinode >= 0 && colAntinode < gridTotalCols &&
-        colAntinode >= 0)
-    {
-        antinodes.insert(rowAntinode * gridTotalRows + colAntinode);
-    }
-}
-
-void FindAntinodes(vector<size_t> const& antennas, Grid const grid, set<size_t>& antinodes)
+void FindAntinodesPart1(vector<size_t> const& antennas, Grid const& grid, set<size_t>& antinodes)
 {
     for (size_t i{}; i < antennas.size(); i++)
     {
@@ -74,7 +54,62 @@ void FindAntinodes(vector<size_t> const& antennas, Grid const grid, set<size_t>&
             {
                 continue;
             }
-            FindAntinode(antennas[i], antennas[j], grid, antinodes);
+            size_t a1          = antennas[i];
+            size_t a2          = antennas[j];
+            long gridTotalRows = grid.first.size() / grid.second;
+            long gridTotalCols = grid.second;
+            long row1          = a1 / grid.second;
+            long row2          = a2 / grid.second;
+            long col1          = a1 % grid.second;
+            long col2          = a2 % grid.second;
+            long rowDist       = row2 - row1;
+            long colDist       = col2 - col1;
+
+            long rowAntinode = row1 + 2 * rowDist;
+            long colAntinode = col1 + 2 * colDist;
+
+            if (rowAntinode < gridTotalRows && rowAntinode >= 0 && colAntinode < gridTotalCols &&
+                colAntinode >= 0)
+            {
+                antinodes.insert(rowAntinode * gridTotalRows + colAntinode);
+            }
+        }
+    }
+}
+
+void FindAntinodesPart2(vector<size_t> const& antennas, Grid const& grid, set<size_t>& antinodes)
+{
+    for (size_t i{}; i < antennas.size(); i++)
+    {
+        for (size_t j{}; j < antennas.size(); j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            size_t a1          = antennas[i];
+            size_t a2          = antennas[j];
+            long gridTotalRows = grid.first.size() / grid.second;
+            long gridTotalCols = grid.second;
+            long row1          = a1 / grid.second;
+            long row2          = a2 / grid.second;
+            long col1          = a1 % grid.second;
+            long col2          = a2 % grid.second;
+            long rowDist       = row2 - row1;
+            long colDist       = col2 - col1;
+
+            long unitRowDist = rowDist / std::gcd(rowDist, colDist);
+            long unitColDist = colDist / std::gcd(rowDist, colDist);
+
+            long rowAntinode = row1 + unitRowDist;
+            long colAntinode = col1 + unitColDist;
+            while (rowAntinode < gridTotalRows && rowAntinode >= 0 && colAntinode < gridTotalCols &&
+                   colAntinode >= 0)
+            {
+                antinodes.insert(rowAntinode * gridTotalRows + colAntinode);
+                rowAntinode += unitRowDist;
+                colAntinode += unitColDist;
+            }
         }
     }
 }
@@ -93,14 +128,17 @@ int main()
         }
     }
 
-    set<size_t> antinodes{};
+    set<size_t> antinodesPart1{};
+    set<size_t> antinodesPart2{};
     for (char f : frequencies)
     {
         vector<size_t> antennaPositions = FindAntennaPositions(f, grid);
-        FindAntinodes(antennaPositions, grid, antinodes);
+        FindAntinodesPart1(antennaPositions, grid, antinodesPart1);
+        FindAntinodesPart2(antennaPositions, grid, antinodesPart2);
     }
 
-    print("Part 1: {}\n", antinodes.size());
+    print("Part 1: {}\n", antinodesPart1.size());
+    print("Part 2: {}\n", antinodesPart2.size());
 
     return 0;
 }
